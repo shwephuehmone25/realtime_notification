475db06815d7d8f1378a81899407cb2d58306a5d
shwephuehmone25 <shwephue7889@gmail.com>
2024-01-31 16:21:47 +0630

    realtime notification with pusher

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SendMessageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/send', [SendMessageController::class, 'index'])->name('send');
+Route::post('/postMessage', [SendMessageController:: class, 'sendMessage'])->name('postMessage');
